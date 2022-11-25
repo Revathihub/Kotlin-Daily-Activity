2d1cdede377ee6c7cb8fff28adf58c60f5223d8c
Revathihub <116789087+Revathihub@users.noreply.github.com>
2022-11-25 15:04:03 +0530

    Delete Employee.kt

--- model/Employee.kt
+++
@@ -1,8 +0,0 @@
-package com.axis.model
-
-class Employee(id:Int,name:String,salary:Int,dept:String){
-    var id:Int=id
-    var name:String=name
-    var salary:Int=salary
-    var department:String=dept
-}
